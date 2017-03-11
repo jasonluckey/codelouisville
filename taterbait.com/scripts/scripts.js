669d3ebc885ce06eb13a16e67f1123c2cc759156
Jason Luckey <jason.luckey@gmail.com>
2017-03-10 20:00:39 -0500

    Updated index

--- taterbait.com/scripts/scripts.js
+++ taterbait.com/scripts/scripts.js
@@ -1,23 +1,19 @@
 //random color javascript
  
- function ran_col() { //function name
- var color = '#'; // hexadecimal starting symbol
- var letters = ['e2620d','f4b342','a2e23b','3ae2c3','e231b9','c993bc','4286f4','f27f52']; //Set colors here
- color += letters[Math.floor(Math.random() * letters.length)]; //put it all together
-  document.body.style.background = color; //make the magic happen
+ function random_color() { 
+ var color = '#'; 
+ var letters = ['e2620d','f4b342','a2e23b','3ae2c3','e231b9','c993bc','4286f4','f27f52']; 
+ color += letters[Math.floor(Math.random() * letters.length)]; 
+  document.body.style.background = color; 
 }
-
 
 //popup a pic
 
-$(document).ready(function() {
-
-	$('.magnific').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		image: {
-			verticalFit: false
-		}
-	});
-
-});
+function popup(mylink, windowname) { 
+	if (! window.focus)return true; 
+	var href; 
+	if (typeof(mylink) == 'string') href=mylink; 
+	else href=mylink.href; 
+	window.open(href, windowname, 'width=300,height=250,scrollbars=yes'); 
+	return false; 
+}
