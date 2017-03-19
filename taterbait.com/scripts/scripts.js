346cd08fe636d096ac94fe1d3becc6a1d3fd942b
JasonLuckey <jason.luckey@gmail.com>
2017-03-19 15:58:38 -0400

    File rewritten for div color change
    Fixed javascript so it's looking for div instead of entire background

--- taterbait.com/scripts/scripts.js
+++ taterbait.com/scripts/scripts.js
@@ -1,10 +1,12 @@
 //random color javascript
  
  function random_color() { 
+ var div = document.getElementById('colorChange');
  var color = '#'; 
  var letters = ['e2620d','f4b342','a2e23b','3ae2c3','e231b9','c993bc','4286f4','f27f52']; 
  color += letters[Math.floor(Math.random() * letters.length)]; 
-  document.body.style.background = color; 
+ div.style.background = color;
+  
 };
 
 //popup a pic
@@ -19,6 +21,7 @@ function popup(mylink, windowname) {
 };
 
 // table stuff
+
 var proxy = 'https://cors-anywhere.herokuapp.com/';
 var myUrl = 'https://www.1988trip.xyz/data.json';
 var finalUrl = proxy + myUrl;
@@ -35,11 +38,10 @@ $.ajax({
 });
 
 // NavBar
-navLinks = document.querySelector('.navNarrow');
-narrowLinks = document.querySelector('.narrowLinks');
 
-navLinks.addEventListener('click', toggle);
-
-function toggle() {
-  narrowLinks.classList.toggle('hidden');
-};
+$(document).ready(function(){
+	$('.toggle').click(function(){
+		$('#nav').toggleClass('open');
+		$('.container').toggleClass('menu-open');
+	});
+});
